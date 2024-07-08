3a2c7fc37158935ad8ff852d400a6617bad6463a
Reikanod <reikanod@yandex.ru>
2024-07-08 14:59:29 +0300

    save

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
-
+import telebot
 from gpt import *
 from util import *
 
 # тут будем писать наш код :)
 
-
-app = ApplicationBuilder().token("telegram-token").build()
+with open(r'G:\Study\Py projects\Keys\tg_chat_key.txt', 'r') as file:
+    app = telebot.TeleBot(file.read()).build()
 app.run_polling()
+
+
+
